6feb5e94f5bf3b051e2391b19fedc35aa1e23a8c
sneha9989 <2005b2.sneha@gmail.com>
2024-03-27 10:01:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import React from 'react';
+import Nikky from './Nikky';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nikky>
+        
+      </Nikky>
+ 
+   
+     
+    </div>
+  );
+}
+
+export default App;
